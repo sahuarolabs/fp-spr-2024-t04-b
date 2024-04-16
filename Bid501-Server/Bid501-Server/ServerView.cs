d384101a757bf1fdc9552d6ab301be74367e6156
nsketron <nsketron@ksu.edu>
2024-04-16 10:32:51 -0500

    server stuff v1

--- Bid501-Server/Bid501-Server/ServerView.cs
+++ Bid501-Server/Bid501-Server/ServerView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Bid501_Server
 {
-    public partial class Form1 : Form
+    public partial class ServerView : Form
     {
-        public Form1()
+        public ServerView()
         {
             InitializeComponent();
         }
