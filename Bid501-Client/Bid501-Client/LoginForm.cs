f9976b2bc95a7a378d376f4c3ebd92a7c9b5122b
ambarret <98120714+ambarret@users.noreply.github.com>
2024-04-16 10:31:16 -0500

    added Login Form to Client

---
+++ Bid501-Client/Bid501-Client/LoginForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bid501_Client
+{
+    public partial class LoginForm: UserControl
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
