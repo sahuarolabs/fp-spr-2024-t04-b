6a60fa4ad29dd9520c8faefd706cd489f6d8c784
nsketron <nsketron@ksu.edu>
2024-04-16 10:42:26 -0500

    server stuff v2

--- Bid501-Server/Bid501-Server/ServerView.Designer.cs
+++ Bid501-Server/Bid501-Server/ServerView.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uxConnectionText = new System.Windows.Forms.TextBox();
+            this.uxConnectionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uxConnectionText
+            // 
+            this.uxConnectionText.Location = new System.Drawing.Point(116, 15);
+            this.uxConnectionText.Name = "uxConnectionText";
+            this.uxConnectionText.ReadOnly = true;
+            this.uxConnectionText.Size = new System.Drawing.Size(100, 20);
+            this.uxConnectionText.TabIndex = 0;
+            // 
+            // uxConnectionLabel
+            // 
+            this.uxConnectionLabel.AutoSize = true;
+            this.uxConnectionLabel.Location = new System.Drawing.Point(13, 18);
+            this.uxConnectionLabel.Name = "uxConnectionLabel";
+            this.uxConnectionLabel.Size = new System.Drawing.Size(97, 13);
+            this.uxConnectionLabel.TabIndex = 1;
+            this.uxConnectionLabel.Text = "Connection Status:";
+            // 
+            // ServerView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uxConnectionLabel);
+            this.Controls.Add(this.uxConnectionText);
+            this.Name = "ServerView";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox uxConnectionText;
+        private System.Windows.Forms.Label uxConnectionLabel;
     }
 }
 
