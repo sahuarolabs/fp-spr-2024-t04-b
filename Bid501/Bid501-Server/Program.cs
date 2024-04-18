99aac86499d0369c9ee1f949da5adc58c4fbbc82
nsketron <nsketron@ksu.edu>
2024-04-18 10:47:09 -0500

    Filled out method signatures in ServerCommCtrl according to UML.

--- Bid501/Bid501-Server/Program.cs
+++ Bid501/Bid501-Server/Program.cs
@@ -10,20 +10,7 @@ namespace Bid501_Server
 {
     internal static class Program
     {
-        public class TestService : WebSocketBehavior
-        {
-            protected override void OnMessage(MessageEventArgs e)
-            {
-                Console.WriteLine("Received from client: " + e.Data);
-
-                Send("Data from server");
-            }
-
-            protected override void OnError(WebSocketSharp.ErrorEventArgs e)
-            {
-                // do nothing
-            }
-        }
+        
 
         /// <summary>
         /// The main entry point for the application.
@@ -34,9 +21,7 @@ namespace Bid501_Server
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Still looking to get this working on multiple computers :)
-            WebSocketServer wss = new WebSocketServer("ws://10.130.160.36:8001");
-            wss.AddWebSocketService<TestService>("/Test");
-            wss.Start();
+            
             //Console.OpenStandardInput();
             //Console.ReadKey(true);
             Application.Run(new ServerView());
--- Bid501/Bid501-Server/ServerCommCtrl.cs
+++ Bid501/Bid501-Server/ServerCommCtrl.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using static Bid501_Server.Program;
 
 namespace Bid501_Server
 {
-    internal class ServerCommCtrl
+
+    //make region for 'services'
+    public class TestService : WebSocketBehavior
     {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Received from client: " + e.Data);
+
+            Send("Data from server");
+        }
+
+        protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+        {
+            // do nothing
+        }
+    }
+
+    public class ServerCommCtrl : WebSocketBehavior
+    {
+
+        //private model Model;
+
+        //private addBidDel AddBid;
+
+        //private Dictionary<user,websocket>
+
+        //private logInDel LogIn;
+
+        public ServerCommCtrl()
+        {
+
+            WebSocketServer wss = new WebSocketServer("ws://10.130.160.36:8001");
+            wss.AddWebSocketService<TestService>("/Test");
+            wss.Start();
+
+        }
+
+        protected void OnOpen()
+        {
+
+        }
+
+        protected void OnMessage(MessageEventArgs e)
+        {
+
+        }
+
+        public void NotifyNewProduct()
+        {
+
+        }
+
+        public void NotfyNewBid()
+        {
+
+        }
+
+        public void EndAuction()
+        {
+
+        }
+
     }
 }
