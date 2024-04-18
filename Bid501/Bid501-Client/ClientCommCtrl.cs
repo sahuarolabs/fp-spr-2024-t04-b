7cb650bd85fb25eecd310d6eaf7798882c868906
Patrick Arnold <patricka02@ksu.edu>
2024-04-18 10:10:00 -0500

    WebSocketSharpNuget added

---
+++ Bid501/Bid501-Client/ClientCommCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace Bid501_Client
+{
+    public class ClientCommCtrl
+    {
+        public WebSocketSharp.WebSocket ws;
+
+        public ClientCommCtrl()
+        {
+            ws = new WebSocketSharp.WebSocket("ws://10.130.160.35:8001/Test");
+            ws = new WebSocketSharp.WebSocket("ws://10.130.160.36:8001/Test");
+            ws.Connect();
+
+            //Check Alive
+            bool conn = false;
+            if (ws.IsAlive)
+            {
+                conn = true;
+            }
+            else
+            {
+                MessageBox.Show("Fuck you");
+            }
+            MessageBox.Show("Connection: " + conn);
+        }
+
+        public void SendMessage(string message)
+        {
+            if (ws.IsAlive)
+            {
+                MessageBox.Show("1");
+                ws.Send(message);
+                Console.WriteLine("Sent Message");
+            }
+            else
+            {
+                MessageBox.Show("0");
+                Console.WriteLine("WebSocket is not connected.");
+            }
+        }
+    }
+}
