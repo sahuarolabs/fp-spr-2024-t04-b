2c3d1a84a7c1e75222c517753894f47e870e304f
Andrew Barrett <ambarret@win2.cis.ksu.edu>
2024-04-18 16:33:01 -0500

    Added Bid,Iproduct,and Model

---
+++ Bid501/Bid501-Shared/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Shared
+{
+    public class Model
+    {
+        List<IProduct> Products;
+
+        public Model()
+        {
+            Products = new List<IProduct>();
+        }
+    }
+}
