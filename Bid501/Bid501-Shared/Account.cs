7d0dbe449022230e93a0af78cb5f92e3f5ec04f9
Patrick Arnold <patricka02@ksu.edu>
2024-04-18 10:44:17 -0500

    Add Library classes

--- Bid501/Bid501-Shared/Account.cs
+++ Bid501/Bid501-Shared/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,18 @@ namespace Bid501_Shared
 {
     public class Account
     {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+        public List<Permission> Permissions { get; set; }
 
+        public Account()
+        {
+        }
+
+        public string Serialize()
+        {
+            //FIXME
+            return "";
+        }
     }
 }
