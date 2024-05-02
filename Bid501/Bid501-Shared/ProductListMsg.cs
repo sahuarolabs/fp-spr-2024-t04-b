2b38ddd3d9223f2ba67625619539b5f27e4337bf
Vojtech Dohnal <dohnavoj@ksu.edu>
2024-05-02 11:34:11 -0500

    Add message objects to Shared

---
+++ Bid501/Bid501-Shared/ProductListMsg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Shared
+{
+    [Serializable]
+    public class ProductListMsg : Message
+    {
+        public List<Product> Products { get; set; }
+
+        public ProductListMsg(List<Product> products) : base(Message.Type.ProductList)
+        {
+            Products = products;
+        }
+    }
+}
